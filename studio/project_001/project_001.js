69e25136192c03b804d23a7646c9b55be32e082a
ramor628 <ramor628@newschool.edu>
2022-02-08 18:46:50 -0500

    organization and p1

--- studio/project_001/project_001.js
+++ studio/project_001/project_001.js
@@ -1,7 +1,16 @@
-function pressed(){
-    var text = document.getElementById("answer").value;
-    if (text === "light"){
-        document.body.style.backgroundColor = "white";
-        document.getElementById("container").style.display = "none";
-    }
+function start(){
+    document.body.style.background = 'black'
+    document.getElementById('container').style.display = 'grid'
+    document.getElementById('intro').style.display = 'none'
+}
+
+function bgWhite(){
+    document.body.style.background = 'white'
+    document.getElementById('container').style.display = 'none'
+    document.getElementById('white').style.display = 'grid'
+    document.getElementById('next').style.display = 'grid'
+}
+
+function pinterestPage(){
+    window.location.href = 'https://www.pinterest.com/thelightplaylist/light/'
 }
