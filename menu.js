8a64e27ac21285cf66f20230b7e7b4339cd11bf8
ramor628 <ramor628@newschool.edu>
2022-02-11 06:38:21 -0500

    edit

---
+++ menu.js
@@ -0,0 +1,9 @@
+document.getElementById('ab').addEventListener('mouseenter',bgBlue)
+
+function bgBlue(){
+    document.body.style.background = '';
+}
+
+/*function bgPink(){
+    document.body.style.fontSize = '#e0098e';
+}*/
