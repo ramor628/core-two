a22796444e82025fe6f8f9e528dda81e896315ce
ramor628 <ramor628@newschool.edu>
2022-02-15 14:21:47 -0500

    p2 & p1

--- menu.js
+++ menu.js
@@ -1,9 +1,42 @@
-document.getElementById('ab').addEventListener('mouseenter',bgBlue)
+/*window.addEventListener("load", function(){
+const bodyElement = document.querySelector('body')
 
-function bgBlue(){
-    document.body.style.background = '';
+function mouseOver() {
+  bodyElement.style.background = "radial-gradient(circle, rgba(9,224,120,1) 10%, rgba(26,35,202,1) 100%)";
 }
 
-/*function bgPink(){
-    document.body.style.fontSize = '#e0098e';
-}*/
+function mouseOut() {
+  bodyElement.style.background = "green";
+}
+
+window.addEventListener("mouseover", mouseOver);
+window.addEventListener("mouseout", mouseOut);
+});*/
+
+window.addEventListener("load", function(){
+    let div;
+    div = document.getElementById('blue');
+  
+  const onMouseMove = (e) =>{
+  
+    div.style.left = e.pageX + 'px';
+    div.style.top = e.pageY + 'px';
+  }
+  
+  document.body.addEventListener('mousemove', onMouseMove);
+});
+
+
+document.getElementById('name').onmouseover = function()
+{pinkCircle()};
+
+document.getElementById('name').onmouseout = function()
+{blueCircle()};
+
+function pinkCircle(){
+    document.getElementById('blue') = '#ff9bd9';
+}
+
+function blueCircle(){
+ document.getElementById('blue') = '#1a23ca';
+}
