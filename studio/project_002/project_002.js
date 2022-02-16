e5718c35440915b453b674e291324ad9c5784ce3
ramor628 <ramor628@newschool.edu>
2022-02-16 17:15:21 -0500

    css, javascript

---
+++ studio/project_002/project_002.js
@@ -0,0 +1,9 @@
+function changeCircle(){
+    document.getElementById('one').style.display = 'none'
+    document.getElementById('two').style.display = 'grid'
+}
+
+function displayVolt(){
+    document.getElementById('first').style.display = 'none'
+    document.getElementById('fence').style.display = 'grid'
+}
