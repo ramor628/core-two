0566e444d72d78f9a363cf52c120177f1c4f57c8
ramor628 <ramor628@newschool.edu>
2022-02-17 20:27:40 -0500

    cursor pointer

--- studio/project_002/project_002.js
+++ studio/project_002/project_002.js
@@ -1,24 +1,24 @@
 function displayVolt(){
-    document.getElementById('first').style.display = 'none'
-    document.getElementById('fence').style.display = 'grid'
-    document.getElementById('box3').style.marginBottom = '5em'
-    document.getElementById('click').style.display = 'none'
+    document.getElementById('first').style.display = 'none';
+    document.getElementById('fence').style.display = 'grid';
+    document.getElementById('box3').style.marginBottom = '5em';
+    document.getElementById('click').style.display = 'none';
 }
 
 function lightMode(){
-    document.body.style.background = 'white'
-    document.getElementById('light').style.background = '#ba0104'
-    document.getElementById('light').style.color = 'white'
-    document.getElementById('night').style.background = 'white'
-    document.getElementById('night').style.color = '#ba0104'
-    document.body.style.color = 'black'
+    document.body.style.background = 'white';
+    document.getElementById('light').style.background = '#ba0104';
+    document.getElementById('light').style.color = 'white';
+    document.getElementById('night').style.background = 'white';
+    document.getElementById('night').style.color = '#ba0104';
+    document.body.style.color = 'black';
 }
 
 function nightMode(){
-    document.body.style.background = 'black'
-    document.getElementById('night').style.background = '#ba0104'
-    document.getElementById('night').style.color = 'black'
-    document.getElementById('light').style.background = 'black'
-    document.getElementById('light').style.color = '#ba0104'
-    document.body.style.color = 'white'
+    document.body.style.background = 'black';
+    document.getElementById('night').style.background = '#ba0104';
+    document.getElementById('night').style.color = 'black';
+    document.getElementById('light').style.background = 'black';
+    document.getElementById('light').style.color = '#ba0104';
+    document.body.style.color = 'white';
 }
