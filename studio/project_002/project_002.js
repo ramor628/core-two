2de1219a3749ec8b976113de1877b7f4b8e0646d
ramor628 <ramor628@newschool.edu>
2022-02-17 19:41:22 -0500

    night/day mode

--- studio/project_002/project_002.js
+++ studio/project_002/project_002.js
@@ -1,9 +1,23 @@
-function changeCircle(){
-    document.getElementById('one').style.display = 'none'
-    document.getElementById('two').style.display = 'grid'
-}
-
 function displayVolt(){
     document.getElementById('first').style.display = 'none'
     document.getElementById('fence').style.display = 'grid'
+    document.getElementById('box3').style.marginBottom = '5em'
+}
+
+function lightMode(){
+    document.body.style.background = 'white'
+    document.getElementById('light').style.background = '#ba0104'
+    document.getElementById('light').style.color = 'white'
+    document.getElementById('night').style.background = 'white'
+    document.getElementById('night').style.color = '#ba0104'
+    document.body.style.color = 'black'
+}
+
+function nightMode(){
+    document.body.style.background = 'black'
+    document.getElementById('night').style.background = '#ba0104'
+    document.getElementById('night').style.color = 'black'
+    document.getElementById('light').style.background = 'black'
+    document.getElementById('light').style.color = '#ba0104'
+    document.body.style.color = 'white'
 }
