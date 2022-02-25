b79b64b07a0c0c04fc91b6ada1bc950eed6a9370
ramor628 <ramor628@newschool.edu>
2022-02-24 23:10:18 -0500

    project 2

---
+++ studio/project_002/javascript/nedry.js
@@ -0,0 +1,6 @@
+function showGate(){
+    document.getElementById('first').style.display = 'none';
+    document.getElementById('fence').style.display = 'grid';
+    document.getElementById('box3').style.marginBottom = '5em';
+    document.getElementById('click').style.display = 'none';
+}
