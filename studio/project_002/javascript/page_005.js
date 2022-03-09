93c69166aa8f0d6fc180f6bac3ba39a9c0b0a44b
ramor628 <ramor628@newschool.edu>
2022-03-09 15:03:46 -0500

    p2

--- studio/project_002/javascript/page_005.js
+++ studio/project_002/javascript/page_005.js
@@ -2,6 +2,6 @@ function rain(){
     var div = document.getElementById('first');
 
     if (document.getElementById('first').style.opacity=='0%'){
-        document.body.style.backgroundImage='url(../img/rain.gif)';
+        document.getElementById('first').style.display= 'none';
     }
 }
