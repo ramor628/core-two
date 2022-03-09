c9bb31f275e41fb3aa615b0284c498133ee6ef3f
ramor628 <ramor628@newschool.edu>
2022-03-08 20:38:11 -0500

    p2

--- studio/project_002/javascript/page_005.js
+++ studio/project_002/javascript/page_005.js
@@ -1,19 +1,7 @@
-/*function showGate(){
-    document.getElementById('first').style.display = 'none';
-    document.getElementById('fence').style.display = 'grid';
-    document.getElementById('box3').style.marginBottom = '5em';
-    document.getElementById('click').style.display = 'none';
-}*/
-
-function clearScreen() {
-    document.getElementById('first').style.display = 'none';
-    document.getElementById('next').style.display= 'block';
-}
-
-function clearPedal(){
+function rain(){
     var div = document.getElementById('first');
 
     if (document.getElementById('first').style.opacity=='0%'){
-        document.getElementById('first').style.display='none';
+        document.body.style.backgroundImage='url(../img/rain.gif)';
     }
 }
